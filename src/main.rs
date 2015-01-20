d00bea9d460afcfd7cb5fa52542f981b74434d4a
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-20 14:31:05 +0100

    Use correct content type

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate iron;
 
+use iron::mime::Mime;
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
     Iron::new(|&: _: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello Rust Warsaw!")))
+        let content_type: Mime = "text/html".parse().unwrap();
+        Ok(Response::with((status::Ok, "Hello Rust Warsaw!")).set(content_type))
     }).listen("localhost:3000").unwrap();
 }
