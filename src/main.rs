14db933f325781b3ea4c7d81ea88340b7ae62734
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-28 22:13:11 +0100

    Port number as integer.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate iron;
 
+use std::io::net::ip::Ipv4Addr;
+
 use iron::mime::Mime;
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
+    let port = 3000;
     Iron::new(|&: _: &mut Request| {
         let content_type: Mime = "text/html".parse().unwrap();
         Ok(Response::with((status::Ok, "Hello Rust Warsaw!")).set(content_type))
-    }).listen("localhost:3000").unwrap();
+    }).listen((Ipv4Addr(127, 0, 0, 1), port)).unwrap();
 }
