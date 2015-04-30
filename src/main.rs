fe326e1623d528245098e966da87d6ea7aaf4017
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-04-30 12:55:20 +0200

    Serve static HTML page.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ extern crate docopt;
 extern crate iron;
 extern crate mount;
 extern crate rustc_serialize;
+extern crate staticfile;
 
 use docopt::Docopt;
 
-use iron::mime::Mime;
 use iron::prelude::*;
-use iron::status;
 use mount::Mount;
+use staticfile::Static;
 
 static USAGE: &'static str = "
 Usage: rustwarsaw [options]
@@ -23,17 +23,12 @@ struct Args {
     flag_port: u16,
 }
 
-fn hello(_: &mut Request) -> IronResult<Response> {
-    let content_type: Mime = "text/html".parse().unwrap();
-    Ok(Response::with((status::Ok, "Hello Rust Warsaw!")).set(content_type))
-}
-
 fn main() {
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.help(true).decode())
         .unwrap_or_else(|e| e.exit());
     println!("Starting server on port {}", args.flag_port);
     let mut mount = Mount::new();
-    mount.mount("/", hello);
+    mount.mount("/", Static::new("www/"));
     Iron::new(mount).http(("127.0.0.1", args.flag_port)).unwrap();
 }
